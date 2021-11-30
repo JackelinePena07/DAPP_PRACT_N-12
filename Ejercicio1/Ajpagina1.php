4dbec6c136f34829fa3dbe43acba253da45d4ec2
AREMI <iremapmedina@gmail.com>
2021-11-30 01:28:04 -0500

    Primer ejercicio propuesto de la practica 12

---
+++ Ejercicio1/Ajpagina1.php
@@ -0,0 +1,18 @@
+<?PHP
+if ($_REQUEST['cod']==1)
+ echo "<strong>Pan:</strong> Alimento básico que se hace de harina, levadura y agua.";
+if ($_REQUEST['cod']==2)
+ echo "<strong>Panetón:</strong> Masa de harina que lleva frutas y pasas, es dulce y se come en navidad.";
+if ($_REQUEST['cod']==3)
+ echo "<strong>Bizcocho:</strong> Es una masa esponjosa que se consigue por la emulsión por batido del huevo y el azúcar. Es una elaboración a partir de harina, azúcar y huevo, pudiendo añadir otros ingredientes como grasas, chocolate, frutos secos o frutas.";
+if ($_REQUEST['cod']==4)
+ echo "<strong>¨Pan de Crema Pastelera:</strong> Pan relleno de crema pastelera que se encuentra a la venta en las instalaciones de la UCSM.";
+if ($_REQUEST['cod']==5)
+ echo "<strong>Baguette:</strong> Pan francés, que es alargado y se suele comer en rodajas con carne, mermelada, o puré de pato.";
+if ($_REQUEST['cod']==6)
+ echo "<strong>Pan pita:</strong> El pan pita es un tipo de pan plano blando, levemente fermentado, de harina de trigo, de consumo en el área del Mediterráneo, especialmente en el Cercano Oriente, ocasionalmente cocido en las paredes del horno y que recuerda a la corteza de la pizza.";
+if ($_REQUEST['cod']==7)
+ echo "<strong>Guagua:</strong> Tipo de pan relleno de manjar, que se come en noviembre, en el día de todos los santos, suele llevar una carita.";
+if ($_REQUEST['cod']==8)
+ echo "<strong>Pan común:</strong> Pan que se come con el adobo en Arequipa, se troza en pequeños pedazos que van al caldo.";
+?>
